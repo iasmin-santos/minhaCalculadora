5618f096a66d16dc1da099a01abdce46ec052aec
Iasmin Evelin Santos <eiasmin.santos@gmail.com>
2024-11-05 09:37:23 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/minhacalculadora/MinhaCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minhacalculadora;
+
+/**
+ *
+ * @author 17396227666
+ */
+public class MinhaCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
